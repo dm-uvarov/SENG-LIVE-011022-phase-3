87ee98b0a5f2707211371abea4b6658f9b242114
Jimmy <dm.a.uvarov@gmail.com>
2022-02-23 14:04:13 -0500

    some work here

--- lectures/01-ruby-fundamentals/veterinary_clinic/lib/cli.rb
+++ lectures/01-ruby-fundamentals/veterinary_clinic/lib/cli.rb
@@ -5,11 +5,40 @@ def initialize_app
 
     # Break Out Activity #1
 
-    # patients = [
-    # { id: 1, species:"dog", name:"Jack", owner:"John Smith", phone: "999-999-9999"},
-    # { id: 2, species:"bird", name:"Mia", owner:"Jane Doe", phone: "888-888-8888"},
-    # ...
-    # ]
+    #  binding.pry
+
+    #1
+    patients = [
+    { id: 1, species:"dog", name:"Jack", owner:"John Smith", phone: "999-999-9999"},
+    { id: 2, species:"bird", name:"Mia", owner:"Jane Doe", phone: "888-888-8888"},
+    {  id: 3 , species:"cat", name:"caty", owner:"John Doe", phone: "123-123-1233"}
+    ]
+
+
+    #2
+    puts "Welcome to vet clinic"
+    puts "please choose option"
+    puts "1 list all patients"
+    puts "0 exit"
+
+    #3
+    user_input = gets.strip.split
+
+        # #if user_input
+        # if user_input == "1"
+        #     patients.each do |patient| 
+        #         puts patient[:species]
+        #     end
+        # end
+
+    case user_input
+        when "1"
+            patients.each {|patient| 
+                puts patient[:species]}
+        when "0"
+            puts "bye"
+    end
+
 
     # Break Out Activity #2
 
